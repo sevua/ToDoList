b96cc0a880d0c8c16341b85b7f04f8495a5b23a2
Vsevolod <koz9va@MacBook-Air-Vsevolod.local>
2025-08-09 09:03:45 +0300

    added some futures

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let tasks = [];
+
 document.addEventListener("input", function(e) {
   if (e.target.className === "taskText") {
     e.target.style.height = "auto";
@@ -5,27 +7,51 @@ document.addEventListener("input", function(e) {
   }
 })
 
-const tasksList = document.querySelector(".tasks");
-const addTaskButton = document.getElementById("addTask");
+const addTaskBtn = document.getElementById('addTask');
+const modal = document.getElementById('modal');
+addTaskBtn.addEventListener('click', function() {
+  modal.classList.remove('hidden');
+});
 
-addTaskButton.addEventListener("click", function(){
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    const textarea = document.createElement("textarea");
-    textarea.className = "taskText";
-    textarea.placeholder = "Введите задачу...";
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "deleteTask";
+function renderTask() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+  tasks.forEach(function(task, index) {
+    const li = document.createElement('li');    
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.done;
+    checkbox.classList.add('taskCheckbox');
+    checkbox.dataset.index = index;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('deleteTask');
+    deleteBtn.dataset.index = index;
+
     li.appendChild(checkbox);
-    li.appendChild(textarea);
-    li.appendChild(deleteButton)
-    tasksList.appendChild(li);
-})
+    li.appendChild(document.createTextNode(task.text));
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+  });
+}
 
+const addTaskButton = document.getElementById('addTaskButton');
+const input = document.getElementById('taskInput');
+
+addTaskButton.addEventListener('click', function(){
+  const value = input.value;
+  value.length !== 0 && value.length <= 20 ?tasks.push({ text: value, done: false })
+ : alert("Некорректный ввод");
+  renderTask();
+  input.value ="";
+  modal.classList.add('hidden');
+});
+
+const tasksList = document.getElementById('taskList');
 tasksList.addEventListener("click", function(event){
     if (event.target.classList.contains("deleteTask")){
-        const deleteTB = event.target.parentElement;
-        deleteTB.remove();
+        const index = event.target.dataset.index;
+        tasks.splice(index, 1);
+        renderTask()
     }
 });
+
